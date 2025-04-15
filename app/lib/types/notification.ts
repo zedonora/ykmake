d7a85206c67b54f96be53c547036ef217c3c77bf
ykkim <ykkim@sdt.inc>
2025-04-16 08:37:05 +0900

    reborn

--- app/lib/types/notification.ts
+++
@@ -1,7 +0,0 @@
-export interface Notification {
-    id: string;
-    title: string;
-    content: string;
-    timestamp: string;
-    isRead: boolean;
-}
--- app/routes/messages.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "@remix-run/react";
-
-export default function MessagesLayout() {
-    return (
-        <div className="container py-8">
-            <Outlet />
-        </div>
-    );
-}
